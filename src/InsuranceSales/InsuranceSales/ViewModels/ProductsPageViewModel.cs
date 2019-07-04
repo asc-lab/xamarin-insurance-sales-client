eb23bb14df397c0fc15ff91d9ea066bbb3cf90b3
Maciej Gos <maciej@maciejgos.com>
2019-07-04 12:20:28 +0200

    Create products page view model

---
+++ src/InsuranceSales/InsuranceSales/ViewModels/ProductsPageViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using InsuranceSales.Models;
+using MvvmHelpers;
+
+namespace InsuranceSales.ViewModels
+{
+    public class ProductsPageViewModel : BaseViewModel
+    {
+        public ObservableRangeCollection<Product> Products { get; set; }
+
+        public ProductsPageViewModel()
+        {
+            Header = "Available products";
+            Products = new ObservableRangeCollection<Product>
+            {
+                new Product
+                {
+                    Name = "Super uber product",
+                    Description = "Super uber description",
+                    Code = "SUP",
+                    Image = "DevImage",
+                    MaxNumberOfInsured = 1,
+                    Covers = new Cover[]
+                    {
+                        new Cover
+                        {
+                            Code = "TEST",
+                            Name = "Test cover",
+                            Optional = false,
+                            SumInsured = 1000L
+                        }
+                    }
+                },
+                new Product
+                {
+                    Name = "Super uber product",
+                    Description = "Super uber description",
+                    Code = "SUP",
+                    Image = "DevImage",
+                    MaxNumberOfInsured = 1,
+                    Covers = new Cover[]
+                    {
+                        new Cover
+                        {
+                            Code = "TEST",
+                            Name = "Test cover",
+                            Optional = false,
+                            SumInsured = 1000L
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
