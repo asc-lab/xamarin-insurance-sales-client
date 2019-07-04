b777b81c9268fb150d44bcf2389bf9033979c73d
Maciej Gos <maciej@maciejgos.com>
2019-07-04 10:09:16 +0200

    Initialize Shell

---
+++ src/InsuranceSales/InsuranceSales/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace InsuranceSales
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
