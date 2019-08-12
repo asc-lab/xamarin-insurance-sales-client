0e15b9d7e2fc8992d370eb493b5cfe26f79688cf
Maciej Gos <maciej@maciejgos.com>
2019-08-12 10:30:51 +0200

    Create view model base

---
+++ src/InsuranceSales/InsuranceSales/ViewModels/ViewModelBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using MvvmHelpers;
+
+namespace InsuranceSales.ViewModels
+{
+    public class ViewModelBase : BaseViewModel
+    {
+        public virtual Task InitializeAsync()
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
