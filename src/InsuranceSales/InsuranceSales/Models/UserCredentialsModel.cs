1b78cc595edad8811d29d9406bd9e52995137c58
Maciej Gos <maciej@maciejgos.com>
2019-08-12 15:32:07 +0200

    Create user credentials

---
+++ src/InsuranceSales/InsuranceSales/Models/UserCredentialsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace InsuranceSales.Models
+{
+    public class UserCredentialsModel
+    {
+        public UserCredentialsModel()
+        {
+        }
+
+        public string Username { get; internal set; }
+        public string Password { get; internal set; }
+    }
+}
