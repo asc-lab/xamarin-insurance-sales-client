bfd340540523b120d41df5392dc8e047629d614e
Maciej Gos <maciej@maciejgos.com>
2019-08-12 15:35:02 +0200

    Derive page from base content page

--- src/InsuranceSales/InsuranceSales/Views/ProductsPage.xaml.cs
+++ src/InsuranceSales/InsuranceSales/Views/ProductsPage.xaml.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-
-using Xamarin.Forms;
+﻿using InsuranceSales.ViewModels;
 
 namespace InsuranceSales.Views
 {
-    public partial class ProductsPage : ContentPage
+    public partial class ProductsPage : ContentPageBase<ProductsPageViewModel>
     {
         public ProductsPage()
         {
             InitializeComponent();
-
-            // Hide navigation bar
-            Shell.SetNavBarIsVisible(this, false);
         }
     }
 }
