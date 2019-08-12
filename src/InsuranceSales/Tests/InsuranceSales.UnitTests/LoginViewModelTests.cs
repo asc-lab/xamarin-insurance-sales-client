39c94a8862cc9aee2e418b2bac23bad89e290401
Maciej Gos <maciej@maciejgos.com>
2019-08-12 10:46:30 +0200

    Create login page view model tests

---
+++ src/InsuranceSales/Tests/InsuranceSales.UnitTests/LoginViewModelTests.cs
@@ -0,0 +1,25 @@
+﻿using InsuranceSales.ViewModels;
+using Xunit;
+
+namespace InsuranceSales.UnitTests
+{
+    public class LoginViewModelTests
+    {
+        [Fact]
+        public void UserCanSignIn()
+        {
+            // Arrange
+            string username = "admin";
+            string password = "admin";
+            var vm = new LoginPageViewModel();
+
+            // Act
+            vm.Username = username;
+            vm.Password = password;
+            vm.SignInCommand.Execute(null);
+
+            // Assert
+            Assert.True(vm.IsSignIn);
+        }
+    }
+}
