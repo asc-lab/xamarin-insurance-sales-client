7c8ef4efb1e36fa40ef026dc4e00c0fb973d2552
Maciej Gos <maciej@maciejgos.com>
2019-08-12 15:31:21 +0200

    Create content page base

---
+++ src/InsuranceSales/InsuranceSales/Views/ContentPageBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using InsuranceSales.ViewModels;
+using MvvmHelpers;
+using Xamarin.Forms;
+
+namespace InsuranceSales.Views
+{
+    public abstract class ContentPageBase<T> : ContentPage where T : ViewModelBase
+    {
+        private bool _isInitialized;
+
+        protected virtual T ViewModel => BindingContext as T;
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (!_isInitialized)
+            {
+                ViewModel.InitializeAsync();
+                _isInitialized = true;
+            }
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+        }
+    }
+}
