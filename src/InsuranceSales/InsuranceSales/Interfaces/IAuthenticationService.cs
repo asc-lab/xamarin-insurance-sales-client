40d405afd3b7d39011189dea160fc3320d01e0f1
Maciej Gos <maciej@maciejgos.com>
2019-08-12 15:30:39 +0200

    Create mock authentication service

---
+++ src/InsuranceSales/InsuranceSales/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using InsuranceSales.Models;
+
+namespace InsuranceSales.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        bool IsAuthenticated();
+        Task AuthenticateAsync(UserCredentialsModel userCredentials);
+    }
+}
---
+++ src/InsuranceSales/InsuranceSales/Services/AuthenticationService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using InsuranceSales.Interfaces;
+using InsuranceSales.Models;
+
+namespace InsuranceSales.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private bool _isAuthenticated;
+
+        public Task AuthenticateAsync(UserCredentialsModel userCredentials)
+        {
+            // TODO: Implement
+            if (userCredentials.Username == "admin" && userCredentials.Password == "admin")
+                _isAuthenticated = true;
+
+            return Task.FromResult(true);
+        }
+
+        public bool IsAuthenticated()
+        {
+            // TODO: Implement
+            return _isAuthenticated;
+        }
+    }
+}
