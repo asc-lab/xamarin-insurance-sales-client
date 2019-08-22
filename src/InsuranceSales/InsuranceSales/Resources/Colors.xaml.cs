730796f25a46706e7a50930efe67e0186e5d3cfd
Maciej Gos <maciej@maciejgos.com>
2019-08-22 13:28:41 +0200

    Merged dictionaries

---
+++ src/InsuranceSales/InsuranceSales/Resources/Colors.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace InsuranceSales.Resources
+{
+    public partial class Colors : ResourceDictionary
+    {
+        public Colors()
+        {
+            InitializeComponent();
+        }
+    }
+}
