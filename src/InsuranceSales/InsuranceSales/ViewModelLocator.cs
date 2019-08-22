309c8b8579e36e5be88706e1b6355ce5c0580583
Maciej Gos <maciej@maciejgos.com>
2019-08-22 12:21:46 +0200

    Use of design time data

---
+++ src/InsuranceSales/InsuranceSales/ViewModelLocator.cs
@@ -0,0 +1,10 @@
+﻿using InsuranceSales.ViewModels;
+
+namespace InsuranceSales
+{
+    public class ViewModelLocator
+    {
+        private static ProductsPageViewModel productsPageViewModel;
+        public static ProductsPageViewModel ProductsPageViewModel => productsPageViewModel ?? (productsPageViewModel = new ProductsPageViewModel());
+    }
+}
--- src/InsuranceSales/InsuranceSales/ViewModels/ProductsPageViewModel.cs
+++ src/InsuranceSales/InsuranceSales/ViewModels/ProductsPageViewModel.cs
@@ -1,68 +1,72 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using InsuranceSales.Models;
 using InsuranceSales.Views;
-using MvvmHelpers;
 using Xamarin.Forms;
 
 namespace InsuranceSales.ViewModels
 {
     public class ProductsPageViewModel : ViewModelBase
     {
-        private ObservableRangeCollection<Product> products;
+        public ObservableCollection<Product> Products { get; set; }
 
-        public ObservableRangeCollection<Product> Products { get => products; set => SetProperty(ref products, value); }
+        public ProductsPageViewModel()
+        {
+            if (DesignMode.IsDesignModeEnabled)
+            {
+                Header = "Available products";
+                Products = new ObservableCollection<Product>
+                {
+                    new Product
+                    {
+                        Name = "Super uber product",
+                        Description = "Super uber description",
+                        Code = "SUP",
+                        Image = "",
+                        MaxNumberOfInsured = 1,
+                        Covers = new Cover[]
+                        {
+                            new Cover
+                            {
+                                Code = "TEST",
+                                Name = "Test cover",
+                                Optional = false,
+                                SumInsured = 1000L
+                            }
+                        }
+                    },
+                    new Product
+                    {
+                        Name = "Super uber product",
+                        Description = "Super uber description",
+                        Code = "SUP",
+                        Image = "",
+                        MaxNumberOfInsured = 1,
+                        Covers = new Cover[]
+                        {
+                            new Cover
+                            {
+                                Code = "TEST",
+                                Name = "Test cover",
+                                Optional = false,
+                                SumInsured = 1000L
+                            }
+                        }
+                    }
+                };
+            }
+        }
 
         public override async Task InitializeAsync()
         {
             Header = "Available products";
 
-            MessagingCenter.Subscribe<LoginPageViewModel>(this, "AUTH_MSG", async (sender) => await LoadData());
+            MessagingCenter.Subscribe<LoginPageViewModel>(this, "AUTH_MSG", (sender) => LoadData().ConfigureAwait(false));
 
             if (!AuthenticationService.IsAuthenticated())
                 await Shell.Current.Navigation.PushModalAsync(new LoginPage());
         }
 
-        private async Task LoadData()
-        {
-            Products = new ObservableRangeCollection<Product>
-            {
-                new Product
-                {
-                    Name = "Super uber product",
-                    Description = "Super uber description",
-                    Code = "SUP",
-                    Image = "DevImage",
-                    MaxNumberOfInsured = 1,
-                    Covers = new Cover[]
-                    {
-                        new Cover
-                        {
-                            Code = "TEST",
-                            Name = "Test cover",
-                            Optional = false,
-                            SumInsured = 1000L
-                        }
-                    }
-                },
-                new Product
-                {
-                    Name = "Super uber product",
-                    Description = "Super uber description",
-                    Code = "SUP",
-                    Image = "DevImage",
-                    MaxNumberOfInsured = 1,
-                    Covers = new Cover[]
-                    {
-                        new Cover
-                        {
-                            Code = "TEST",
-                            Name = "Test cover",
-                            Optional = false,
-                            SumInsured = 1000L
-                        }
-                    }
-                }
-            };
-        }
+        private Task LoadData() => Task.CompletedTask;
     }
 }
