3a6ad8edff7be470c928c22afb8452df22f95963
Maciej Gos <maciej@maciejgos.com>
2019-08-22 13:33:00 +0200

    Company header control

---
+++ src/InsuranceSales/InsuranceSales/Controls/CompanyHeader.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace InsuranceSales.Controls
+{
+    public partial class CompanyHeader : ContentView
+    {
+        public CompanyHeader()
+        {
+            InitializeComponent();
+        }
+    }
+}
