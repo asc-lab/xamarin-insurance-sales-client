f31d3a8d8afb03d57770f86f1acea3001c3487c4
Maciej Gos <maciej@maciejgos.com>
2019-11-07 19:15:37 +0100

    Create Network manager

---
+++ src/InsuranceSales/InsuranceSales/Services/NetworkManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using InsuranceSales.Interfaces;
+using InsuranceSales.Models;
+using Refit;
+
+namespace InsuranceSales.Services
+{
+    public class NetworkManager
+    {
+        private readonly IProductsService _productsService;
+
+        public NetworkManager()
+        {
+            _productsService = RestService.For<IProductsService>(AppSettings.backendUrl);
+        }
+
+        public Task<IEnumerable<Product>> GetProducts()
+        {
+            return _productsService.Fetch();
+        }
+    }
+}
