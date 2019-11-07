41775bec788db005b7f3b5168259492095b939d8
Maciej Gos <maciej@maciejgos.com>
2019-11-07 19:16:09 +0100

    Replace usage of interface by network manager

--- src/InsuranceSales/InsuranceSales/ViewModels/ProductsPageViewModel.cs
+++ src/InsuranceSales/InsuranceSales/ViewModels/ProductsPageViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
-using InsuranceSales.Interfaces;
 using InsuranceSales.Models;
 using InsuranceSales.Views;
 using MvvmHelpers;
@@ -10,14 +9,10 @@ namespace InsuranceSales.ViewModels
 {
     public class ProductsPageViewModel : ViewModelBase
     {
-        private readonly IProductsService service;
-
         public ObservableRangeCollection<Product> Products { get; set; } = new ObservableRangeCollection<Product>();
 
         public ProductsPageViewModel()
         {
-            service = DependencyService.Get<IProductsService>();
-
             if (DesignMode.IsDesignModeEnabled)
             {
                 Products = new ObservableRangeCollection<Product>
@@ -75,12 +70,12 @@ namespace InsuranceSales.ViewModels
         private async Task LoadData()
         {
             IsBusy = true;
-            var results = await service.FetchAsync();
-            if(results.Count() > 0)
+            var products = await NetworkManager.GetProducts();
+            if(products.Count() > 0)
             {
                 IsBusy = false;
                 Products.Clear();
-                Products.AddRange(results);
+                Products.AddRange(products);
             }
         }
     }
