ff334cb08a163a5eaf7c3bce10772f40dd5ed18c
Bartosz <elitarform@gmail.com>
2019-12-06 16:42:20 +0100

    Update mock products

--- src/InsuranceSales/InsuranceSales/Services/MockProductsService.cs
+++ src/InsuranceSales/InsuranceSales/Services/MockProductsService.cs
@@ -12,188 +12,144 @@ namespace InsuranceSales.Services
     public class MockProductsService : IProductsService
     {
         private readonly IList<ProductModel> _products = new List<ProductModel>
+        {
+            new ProductModel
             {
-                new ProductModel
+                Id = Guid.NewGuid(),
+                Name = "Happy Driver",
+                Description = "Car insurance",
+                Code = "HD",
+                Image = "https://www.rabbiavishafran.com/wp-content/uploads/2018/08/uber-driver-2-624x416.jpg",
+                MaxNumberOfInsured = 500L,
+                Covers = new[]
                 {
-                    Id = Guid.NewGuid(),
-                    Name = "Happy Driver",
-                    Description = "Car insurance",
-                    Code = "HD",
-                    Image = "https://www.rabbiavishafran.com/wp-content/uploads/2018/08/uber-driver-2-624x416.jpg",
-                    MaxNumberOfInsured = 500L,
-                    Questions = new[]
+                    new CoverModel
                     {
-                        new QuestionModel
-                        {
-                            Id = Guid.NewGuid(),
-                            Code = "",
-                            Index = 1,
-                            Text = $"QUESTION {new Random().Next()}" ,
-                            Type = QuestionTypeEnum.Choice,
-                            Choices = new[]
-                            {
-                                new ChoiceModel
-                                {
-                                    Label = "label",
-                                    Code = "code"
-                                },
-                                new ChoiceModel
-                                {
-                                    Label = "label",
-                                    Code = "code"
-                                },
-                                new ChoiceModel
-                                {
-                                    Label = "label",
-                                    Code = "code"
-                                },
-                            },
-                        },
-                        new QuestionModel
-                        {
-                            Id = Guid.NewGuid(),
-                            Code = "QUESTION",
-                            Index = 2,
-                            Text = $"QUESTION {new Random().Next()}" ,
-                            Type = QuestionTypeEnum.Numeric,
-                            Choices = Array.Empty<ChoiceModel>()
-                        },
-                        new QuestionModel
-                        {
-                            Id = Guid.NewGuid(),
-                            Code = "QUESTION",
-                            Index = 3,
-                            Text = $"QUESTION {new Random().Next()}" ,
-                            Type = QuestionTypeEnum.Text,
-                            Choices = Array.Empty<ChoiceModel>()
-                        },
+                        Id = Guid.NewGuid(),
+                        Code = "AC",
+                        Name = "Assistance",
+                        Optional = true,
+                        SumInsured = new Random().Next()
                     },
-                    Covers = new[]
-                    {
-                        new CoverModel
-                        {
-                            Id = Guid.NewGuid(),
-                            Code = "TEST",
-                            Name = "Fire",
-                            Optional = false,
-                            SumInsured = new Random().Next()
-                        },
-                        new CoverModel
-                        {
-                            Id = Guid.NewGuid(),
-                            Code = "TEST",
-                            Name = "Flood",
-                            Optional = false,
-                            SumInsured = new Random().Next()
-                        },
-                    }
                 },
-                new ProductModel
+                Questions = new[]
                 {
-                    Id = Guid.NewGuid(),
-                    Name = "Happy farm",
-                    Description = " Farm insurance",
-                    Code = "HF",
-                    Image = "https://d279m997dpfwgl.cloudfront.net/wp/2015/05/0522_farmers.jpg",
-                    MaxNumberOfInsured = 1,
-                    Covers = new[]
+                    new QuestionModel
                     {
-                        new CoverModel
-                        {
-                            Id = Guid.NewGuid(),
-                            Code = "TEST",
-                            Name = "Fire",
-                            Optional = false,
-                            SumInsured = new Random().Next()
-                        },
-                        new CoverModel
-                        {
-                            Id = Guid.NewGuid(),
-                            Code = "TEST",
-                            Name = "Flood",
-                            Optional = false,
-                            SumInsured = new Random().Next()
-                        },
-                        new CoverModel
-                        {
-                            Id = Guid.NewGuid(),
-                            Code = "TEST",
-                            Name = "Fire",
-                            Optional = false,
-                            SumInsured = new Random().Next()
-                        },
-                        new CoverModel
-                        {
-                            Id = Guid.NewGuid(),
-                            Code = "TEST",
-                            Name = "Flood",
-                            Optional = false,
-                            SumInsured = new Random().Next()
-                        },
-                        new CoverModel
-                        {
-                            Id = Guid.NewGuid(),
-                            Code = "TEST",
-                            Name = "Fire",
-                            Optional = false,
-                            SumInsured = new Random().Next()
-                        },
-                        new CoverModel
-                        {
-                            Id = Guid.NewGuid(),
-                            Code = "TEST",
-                            Name = "Flood",
-                            Optional = false,
-                            SumInsured = new Random().Next()
-                        },
-                        new CoverModel
-                        {
-                            Id = Guid.NewGuid(),
-                            Code = "TEST",
-                            Name = "Fire",
-                            Optional = false,
-                            SumInsured = new Random().Next()
-                        },
-                        new CoverModel
-                        {
-                            Id = Guid.NewGuid(),
-                            Code = "TEST",
-                            Name = "Flood",
-                            Optional = false,
-                            SumInsured = new Random().Next()
-                        },
+                        Id = Guid.NewGuid(),
+                        Code = "NUM_OF_CLAIMS",
+                        Index = 1,
+                        Text = "Number of claims in last 5 years",
+                        Type = QuestionTypeEnum.Numeric,
                     },
-                    Questions = new[]
+                },
+            },
+            new ProductModel
+            {
+                Id = Guid.NewGuid(),
+                Name = "Happy farm",
+                Description = " Farm insurance",
+                Code = "HF",
+                Image = "https://d279m997dpfwgl.cloudfront.net/wp/2015/05/0522_farmers.jpg",
+                MaxNumberOfInsured = 1,
+                Covers = new[]
+                {
+                    new CoverModel
                     {
-                        new QuestionModel
+                        Id = Guid.NewGuid(),
+                        Code = "FR",
+                        Name = "Fire",
+                        Optional = false,
+                        SumInsured = new Random().Next()
+                    },
+                    new CoverModel
+                    {
+                        Id = Guid.NewGuid(),
+                        Code = "FL",
+                        Name = "Flood",
+                        Optional = false,
+                        SumInsured = new Random().Next()
+                    },
+                    new CoverModel
+                    {
+                        Id = Guid.NewGuid(),
+                        Code = "TH",
+                        Name = "Theft",
+                        Optional = false,
+                        SumInsured = new Random().Next()
+                    },
+                    new CoverModel
+                    {
+                        Id = Guid.NewGuid(),
+                        Code = "AC",
+                        Name = "Assistance",
+                        Optional = false,
+                        SumInsured = new Random().Next()
+                    },
+                },
+                Questions = new[]
+                {
+                    new QuestionModel
+                    {
+                        Id = Guid.NewGuid(),
+                        Code = "CT",
+                        Index = 1,
+                        Text = "Cultivation type",
+                        Type = QuestionTypeEnum.Choice,
+                        Choices = new[]
                         {
-                            Id = Guid.NewGuid(),
-                            Code = "NO",
-                            Index = 1,
-                            Text = $"QUESTION {new Random().Next()}",
-                            Type = QuestionTypeEnum.Choice,
-                            Choices = new[]
+                            new ChoiceModel
                             {
-                                new ChoiceModel
-                                {
-                                    Label = "Choice 1",
-                                    Code = "C1"
-                                },
-                                new ChoiceModel
-                                {
-                                    Label = "Choice 2",
-                                    Code = "C2"
-                                },
-                                new ChoiceModel
-                                {
-                                    Label = "Choice 3",
-                                    Code = "C3"
-                                },
+                                Label = "Crop",
+                                Code = "ZB"
+                            },
+                            new ChoiceModel
+                            {
+                                Label = "Vegetable",
+                                Code = "KW"
                             },
                         },
                     },
-                }
-            };
+                    new QuestionModel
+                    {
+                        Id = Guid.NewGuid(),
+                        Code = "AREA",
+                        Index = 3,
+                        Text = "Area",
+                        Type = QuestionTypeEnum.Numeric,
+                    },
+                    new QuestionModel
+                    {
+                        Id = Guid.NewGuid(),
+                        Code = "NUM_OF_CLAIMS",
+                        Index = 2,
+                        Text = "Number of claims in last 5 years " ,
+                        Type = QuestionTypeEnum.Numeric,
+                    },
+                    new QuestionModel
+                    {
+                        Id = Guid.NewGuid(),
+                        Code = "FLOOD_AREA",
+                        Index = 4,
+                        Text = "Located in flood risk area",
+                        Type = QuestionTypeEnum.Choice,
+                        Choices = new[]
+                        {
+                            new ChoiceModel
+                            {
+                                Label = "Yes",
+                                Code = "YES"
+                            },
+                            new ChoiceModel
+                            {
+                                Label = "No",
+                                Code = "NO"
+                            },
+                        },
+                    },
+                },
+            }
+        };
 
         public async Task<IEnumerable<ProductModel>> FetchAsync()
         {
