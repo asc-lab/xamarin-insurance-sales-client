3dba216b1f3526bbdc0976c31fad981e8abb458e
Bartosz <elitarform@gmail.com>
2019-12-06 16:42:08 +0100

    Create CoverTableView

---
+++ src/InsuranceSales/InsuranceSales/Controls/CoverTableView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using InsuranceSales.Models.Product;
+using System.Collections.Generic;
+using System.Globalization;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace InsuranceSales.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CoverTableView
+    {
+        public CoverTableView() => InitializeComponent();
+
+        protected override void OnBindingContextChanged()
+        {
+            if (!(BindingContext is IList<CoverModel> covers))
+                return;
+
+            var indexHeader = new Label { Text = "Index" };
+            var nameHeader = new Label { Text = nameof(CoverModel.Name) };
+            var sumInsuredHeader = new Label { Text = nameof(CoverModel.SumInsured) };
+
+            CoverTableGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            CoverTableGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            CoverTableGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+            CoverTableGrid.Children.Add(indexHeader, 0, 0);
+            CoverTableGrid.Children.Add(nameHeader, 1, 0);
+            CoverTableGrid.Children.Add(sumInsuredHeader, 2, 0);
+
+            for (var i = 0; i < covers.Count; i++)
+            {
+                var cover = covers[i];
+                var indexLabel = new Label { Text = i.ToString(CultureInfo.CurrentCulture) };
+                var nameLabel = new Label { Text = cover.Name };
+                var sumInsuredLabel = new Label { Text = cover.SumInsured.ToString() };
+
+                CoverTableGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+
+                CoverTableGrid.Children.Add(indexLabel, 0, i + 1);
+                CoverTableGrid.Children.Add(nameLabel, 1, i + 1);
+                CoverTableGrid.Children.Add(sumInsuredLabel, 2, i + 1);
+            }
+            base.OnBindingContextChanged();
+        }
+    }
+}
